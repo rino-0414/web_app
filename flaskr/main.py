7de48662a057905df784fc678e0ac38fce15ab92
rino-0414 <ryoyamazaki2004@outlook.com>
2025-01-06 12:23:36 +0900

    更新

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,12 +3,8 @@ from flask import render_template
 
 @app.route('/')
 def index():
-
-    test = {'name': 'Rino', 'age':'admin'}
-
     return render_template(
         'index.html',
-        test=test
     )
 
 @app.route('/form')
