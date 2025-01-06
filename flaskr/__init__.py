e5612757bc5bf24fa1105fffb584d65ca886747e
rino-0414 <ryoyamazaki2004@outlook.com>
2025-01-06 12:20:24 +0900

    更新

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,24 @@
 from flask import Flask
-app = Flask(__name__)
-import flaskr.main
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    # データベース設定
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0414@localhost/calendar_app'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # SQLAlchemyの初期化
+    db.init_app(app)
+
+    # テーブル作成
+    with app.app_context():
+        db.create_all()
+
+    # ブループリントの登録
+    from .main import bp
+    app.register_blueprint(bp)
+
+    return app
